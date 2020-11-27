d40ecd82ae8c51a243fc322ef40a1640781f63b5
Franciszek Madej <f.madej@protonmail.com>
2020-11-28 00:57:35 +0100

    fewfkjwe

--- api/find.py
+++ api/find.py
@@ -6,5 +6,5 @@ from pathlib import Path
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    csv = pd.read_csv("/scraper/outputs/iosco.csv")
+    csv = pd.read_csv(Path(__file__).parent().parent() / "scraper/outputs/iosco.csv")
     return csv.to_json()
