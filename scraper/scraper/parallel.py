7d89ad6d70bbc5a9db1bacdf421610628fed3073
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-11-28 15:48:38 +0100

    fix(scraper): parallel loading bars

--- scraper/scraper/parallel.py
+++ scraper/scraper/parallel.py
@@ -5,22 +5,20 @@ from tqdm.auto import tqdm
 
 
 def parallel_map(function, iterable, num_jobs=32):
-    with tqdm_joblib(iterable) as iterator:
+    with tqdm_joblib(tqdm(iterable)):
         pool = Parallel(n_jobs=num_jobs)
         return [
             site
             for site in pool(
                 delayed(function)(item)
-                for item in iterator
+                for item in iterable
             )
             if site is not None
         ]
 
 
 @contextlib.contextmanager
-def tqdm_joblib(iterable):
-    tqdm_object = tqdm(iterable)
-    
+def tqdm_joblib(tqdm_object):    
     class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
