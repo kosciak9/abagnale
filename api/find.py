fc11d2a93a7e15738fbcb7fabcbc50be97baacd6
Franciszek Madej <f.madej@protonmail.com>
2020-11-28 01:09:19 +0100

    strf

--- api/find.py
+++ api/find.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 import pandas as pd
 from pathlib import Path
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/', defaults={'path': ''}, methods=["POST"])
+@app.route('/<path:path>', methods=["POST"])
 def catch_all(path):
     csv = pd.read_csv(Path(__file__).parent.parent / "scraper/outputs/iosco.csv")
     return jsonify(csv.transpose().to_dict())
