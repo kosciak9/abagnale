1a8547fe943ae8fbf15cf31a73bbeff74874b27e
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-11-28 12:32:28 +0100

    feature(scraper): make web indexable by url

--- scraper/scraper/web.py
+++ scraper/scraper/web.py
@@ -5,7 +5,14 @@ from scraper.site import Site
 
 class Web:
     def __init__(self, sites):
-        self.sites = frozenset(sites)
+        self.sites = (
+            sites
+            if isinstance(sites, dict)
+            else {
+                site.url: site
+                for site in sites
+            }
+        )
     
 
     @classmethod
@@ -31,9 +38,14 @@ class Web:
     def merge(cls, *args):
         '''Merge multiple webs, making sure not to duplicate sites'''
         return cls(sites=reduce(
-            lambda a, b: a.union(b),
-            (arg.sites for arg in args)
+            lambda a, b: a.union(b.values()),
+            (arg.sites for arg in args),
+            set()
         ))
+    
+
+    def __getitem__(self, url):
+        return self.sites[url]
     
 
     def __repr__(self):
